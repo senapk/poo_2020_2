254f71a0860d56f5fd0dfe5d78e32b574e0fb5c4
David Sena <sena.ufc@gmail.com>
2020-12-15 09:30:32 -0300

    up

--- s04e02_arraylist/ArrayTest.java
+++ s04e02_arraylist/ArrayTest.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import javax.swing.plaf.synth.SynthToolBarUI;
-
 import java.util.ArrayList;
 
 class ArrayT{
@@ -43,7 +41,7 @@ class ListaTest{
     }
 }   
 
-class ArrayListTest{
+class ArrayListTest1{
     public static void main(String[] args) {
         List<String> cores = Arrays.asList("blue", "yellow", "pink", "silver", "gold");
         
@@ -93,3 +91,63 @@ class ArrayListTest{
         System.out.println(times);
     }
 }   
+
+class Pessoa{
+    String nome;
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+    public String toString(){
+        return nome;
+    }
+}
+
+public class ArrayTest{
+    public static void main(String[] args) {
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(new Pessoa("Jose"));
+        Pessoa maria = new Pessoa("Maria");
+        pessoas.add(maria);
+        pessoas.add(new Pessoa("Jesus"));
+        System.out.println(pessoas);
+
+        ArrayList<Pessoa> amigos = new ArrayList<>(Arrays.asList(new Pessoa("Jose"),
+                                        new Pessoa("Antonio"), new Pessoa("Pedro"), 
+                                        new Pessoa("Ricardo")));
+        System.out.println(amigos);
+
+        //amigos.addAll(pessoas);
+        for(Pessoa pessoa : pessoas)
+            amigos.add(pessoa);
+
+        System.out.println(amigos);
+
+        //to com raiva do ricardo
+        amigos.remove(3);
+
+        System.out.println(amigos);
+
+        for(int i = 0; i < amigos.size(); i++){
+            if(amigos.get(i).nome.equals("Maria")){
+                amigos.remove(i);
+                break;// EH MUITO IMPORTANTE
+            }
+        }
+        System.out.println(amigos);
+
+        amigos.add(0, new Pessoa("Bebe Yoda"));
+
+        System.out.println(amigos);
+
+        for(int i = 0; i < amigos.size(); i++){
+            if(amigos.get(i).nome.equals("Bebe Yoda")){
+                //opcao 1 - substitui o objeto pessoa por outro
+                //amigos.set(i, new Pessoa("Grogu"));
+                //opcao 2 - aproveita o objeto e substitui o nome
+                amigos.get(i).nome = "Grogu";
+                break;// EH MUITO IMPORTANTE
+            }
+        }
+        System.out.println(amigos);
+    }
+} 
